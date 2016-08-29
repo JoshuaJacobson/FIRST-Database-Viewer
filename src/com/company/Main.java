f450644a08490f16ef479bd2f4dda5d4d3f85214
josh <fudbuds@gmail.com>
2016-08-29 14:09:53 -0400

    Added csdsConverter() method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,14 @@ public class Main {
 
     public static void main(String[] args) {
         Main m = new Main();
-        m.csdsConverter("");
+        m.csdsConverter("cvjhvghvjhjvh");
     }
 
     public Integer[] csdsConverter(String csds) {
         Integer[] i = new Integer[1000];
-
-
+        String output;
+        output = csds.substring(0, 1);
+        i[0] = Integer.parseInt(output);
 
         return i;
     }
