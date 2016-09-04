76058f3375e10de7eb8f3e3ff9053cd6a1cf7589
josh <fudbuds@gmail.com>
2016-09-03 23:08:33 -0400

    Finished switch in csds conversion.

--- src/com/company/JoEvent.java
+++ src/com/company/JoEvent.java
@@ -94,6 +94,12 @@ public class JoEvent {
     public int teleop_rough_terrain_points = 0;
     public int teleop_low_bar = 0;
     public int teleop_low_bar_points = 0;
+    public int bonus_points = 0;
+    public int breach = 0;
+    public int challenge = 0;
+    public int capture = 0;
+    public String result = "";
+    private boolean challenged = false;
 
     public JoEvent(String csds) throws Exception {
         String[] i = new String[1000];
@@ -217,7 +223,143 @@ public class JoEvent {
                 case 20: //Portcullis
                     total_points = total_points + auto_defence_cross;
 
+                    defence_points = defence_points + auto_defence_cross;
+                    portcullis++;
+                    portcullis_points = portcullis_points + auto_defence_cross;
 
+                    auto_defence_points = auto_defence_points + auto_defence_cross;
+                    auto_portcullis++;
+                    auto_portcullis_points = auto_portcullis_points + auto_defence_cross;
+                    break;
+                case 21: //Cheval De Frise
+                    total_points = total_points + auto_defence_cross;
+
+                    defence_points = defence_points + auto_defence_cross;
+                    cheval_de_frise++;
+                    cheval_de_frise_points = cheval_de_frise_points + auto_defence_cross;
+
+                    auto_defence_points = auto_defence_points + auto_defence_cross;
+                    auto_cheval_de_frise++;
+                    auto_cheval_de_frise_points = auto_cheval_de_frise_points + auto_defence_cross;
+                    break;
+                case 22: //Ramparts
+                    total_points = total_points + auto_defence_cross;
+
+                    defence_points = defence_points + auto_defence_cross;
+                    ramparts++;
+                    ramparts_points = ramparts_points + auto_defence_cross;
+
+                    auto_defence_points = auto_defence_points + auto_defence_cross;
+                    auto_ramparts++;
+                    auto_ramparts_points = auto_ramparts_points + auto_defence_cross;
+                    break;
+                case 23: //Moat
+                    total_points = total_points + auto_defence_cross;
+
+                    defence_points = defence_points + auto_defence_cross;
+                    moat++;
+                    moat_points = moat_points + auto_defence_cross;
+
+                    auto_defence_points = auto_defence_points + auto_defence_cross;
+                    auto_moat++;
+                    auto_moat_points = auto_moat_points + auto_defence_cross;
+                    break;
+                case 24: //Drawbridge
+                    total_points = total_points + auto_defence_cross;
+
+                    defence_points = defence_points + auto_defence_cross;
+                    drawbridge++;
+                    drawbridge_points = drawbridge_points + auto_defence_cross;
+
+                    auto_defence_points = auto_defence_points + auto_defence_cross;
+                    auto_drawbridge++;
+                    auto_drawbridge_points = auto_drawbridge_points + auto_defence_cross;
+                    break;
+                case 25: //Sally Port
+                    total_points = total_points + auto_defence_cross;
+
+                    defence_points = defence_points + auto_defence_cross;
+                    sally_port++;
+                    sally_port_points = sally_port_points + auto_defence_cross;
+
+                    auto_defence_points = auto_defence_points + auto_defence_cross;
+                    auto_sally_port++;
+                    auto_sally_port_points = auto_sally_port_points + auto_defence_cross;
+                    break;
+                case 26: //Rock Wall
+                    total_points = total_points + auto_defence_cross;
+
+                    defence_points = defence_points + auto_defence_cross;
+                    rock_wall++;
+                    rock_wall_points = rock_wall_points + auto_defence_cross;
+
+                    auto_defence_points = auto_defence_points + auto_defence_cross;
+                    auto_rock_wall++;
+                    auto_rock_wall_points = auto_rock_wall_points + auto_defence_cross;
+                    break;
+                case 27: //Rough Terrain
+                    total_points = total_points + auto_defence_cross;
+
+                    defence_points = defence_points + auto_defence_cross;
+                    rough_terrain++;
+                    rough_terrain_points = rough_terrain_points + auto_defence_cross;
+
+                    auto_defence_points = auto_defence_points + auto_defence_cross;
+                    auto_rough_terrain++;
+                    auto_rough_terrain_points = auto_rough_terrain_points + auto_defence_cross;
+                    break;
+                case 28: //Low Bar
+                    total_points = total_points + auto_defence_cross;
+
+                    defence_points = defence_points + auto_defence_cross;
+                    low_bar++;
+                    low_bar_points = low_bar_points + auto_defence_cross;
+
+                    auto_defence_points = auto_defence_points + auto_defence_cross;
+                    auto_low_bar++;
+                    auto_low_bar_points = auto_low_bar_points + auto_defence_cross;
+                    break;
+                case 29: //Reach Defence
+                    total_points = total_points + auto_defence_reach;
+                    defence_points = defence_points + auto_defence_reach;
+                    auto_defence_points = auto_defence_points + auto_defence_reach;
+                    break;
+                case 30: //Outer Works Breach
+                    total_points = total_points + outer_works_breach;
+                    bonus_points = bonus_points + outer_works_breach;
+                    breach = breach + outer_works_breach;
+                    break;
+                case 31: //Challenge
+                    challenged = true;
+                    total_points = total_points + challenge_points;
+                    bonus_points = bonus_points + challenge_points;
+                    challenge = challenge_points;
+                    break;
+                case 32: //Scale
+                    if (challenged = true) {
+                        total_points = total_points - challenge_points;
+                        bonus_points = bonus_points - challenge_points;
+                    }
+                    total_points = total_points + scaling;
+                    bonus_points = bonus_points + scaling;
+                    challenge = scaling;
+                    break;
+                case 33: //Capture
+                    total_points = total_points + tower_capture;
+                    bonus_points = bonus_points + tower_capture;
+                    capture = capture + tower_capture;
+                    break;
+                case 40: //Win
+                    result = "WIN";
+                    break;
+                case 41: //Loss
+                    result = "LOSS";
+                    break;
+                case 42: //Tie
+                    result = "TIE";
+                    break;
+                default:
+                    break;
             }
         }
     }
